98c5c947d4e689f77ce00a6a489d067cfa1f09c2
Adam Seering <aseering@gmail.com>
2013-03-18 00:20:42 -0400

    Some more tweaks to setup.py.  Still doesn't pull dependencies properly; not sure why not...

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='app_store',
+setup(
+      name='app_store',
       version=0.5,
       description="App Store for Vertica",
-      zip_safe=False,
       author="Adam Seering",
       author_email="aseering@vertica.com",
       packages=find_packages(),
-      include_package_data=True,
       install_requires=['django>=1.4',
                         'django-userena'],
-      classifiers = ['Development Status :: 4 - Beta',
-                     'Environment :: Web Environment',
-                     'Framework :: Django',
-                     'Intended Audience :: Developers',
-                     'Operating System :: OS Independent',
-                     'Programming Language :: Python'],
 )
